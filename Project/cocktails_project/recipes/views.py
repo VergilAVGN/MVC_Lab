7f2d14134f32d416314b0d302242cfa377fefcb9
VergilAVGN <frimanrost@gmail.com>
2026-05-08 16:36:07 +0200

    LAB_9

--- Project/cocktails_project/recipes/views.py
+++ Project/cocktails_project/recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from .forms import RecipeForm
 
 import recipes
 
@@ -11,35 +12,32 @@ from .models import Recipe
 @login_required
 def recipe_create(request):
     if request.method == 'POST':
-        Recipe.objects.create(
-            user=request.user,
-            name=request.POST.get('name'),
-            ingredients=request.POST.get('ingredients'),
-            instructions=request.POST.get('instructions'),
-            image=request.FILES.get('image')
-        )
-        return redirect('recipe_list')
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe = form.save(commit=False)
+            recipe.user = request.user
+            recipe.save()
+            return redirect('recipe_list')
+    else:
+        form = RecipeForm()
 
-    return render(request, 'recipes/form.html')
+    return render(request, 'recipes/form.html', {'form': form})
+
 #edit recipe
 @login_required
 def recipe_update(request, id):
     recipe = get_object_or_404(Recipe, id=id, user=request.user)
+    if request.method == "POST":
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.user = request.user
+            obj.save()
+            return redirect("recipe_list")
+    else:
+        form = RecipeForm(instance=recipe)
+    return render(request, "recipes/form.html", {"form": form, "recipe": recipe})
 
-    if request.method == 'POST':
-        recipe.name = request.POST.get('name')
-        recipe.ingredients = request.POST.get('ingredients')
-        recipe.instructions = request.POST.get('instructions')
-        if request.FILES.get('image'):
-            recipe.image = request.FILES.get('image')
-
-        if request.POST.get('remove_image'):
-            recipe.image.delete()
-            recipe.image = None
-        recipe.save()
-        return redirect('recipe_list')
-
-    return render(request, 'recipes/form.html', {'recipe': recipe})
 #delete recipe
 @login_required
 def recipe_delete(request, id):
