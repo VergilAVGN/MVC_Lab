b2488f4001d3afbdb5d407c3b4d61ed2dc030e1a
VergilAVGN <frimanrost@gmail.com>
2026-05-15 16:29:07 +0200

    Lab_10

--- Project/cocktails_project/recipes/urls.py
+++ Project/cocktails_project/recipes/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.recipe_list, name='recipe_list'),
+    path('recipe/<int:id>/', views.recipe_detail, name='recipe_detail'),
     path('add/', views.recipe_create, name='recipe_create'),
     path('edit/<int:id>/', views.recipe_update, name='recipe_update'),
     path('delete/<int:id>/', views.recipe_delete, name='recipe_delete'),
